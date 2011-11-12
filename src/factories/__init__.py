773993de44df2bb04b5cf7df772613da42c6d69c
Nathan Yergler <nathan@eventbrite.com>
2011-11-12 12:05:17 -0800

    Don't require docstrings on blueprints.
    This doesn't mean it's not a good idea, just that it's not always
    necessary.

--- src/factories/__init__.py
+++ src/factories/__init__.py
@@ -104,7 +104,7 @@ def blueprint(model):
         # FactoryMetaclass.
         _wrapped_func._factories_model = model_cls
         _wrapped_func._factories_blueprint = True
-        _wrapped_func.__doc__ = 'Blueprint: ' + _wrapped_func.__doc__
+        _wrapped_func.__doc__ = 'Blueprint: %s' % (_wrapped_func.__doc__)
 
         return _wrapped_func
     return _decorator
